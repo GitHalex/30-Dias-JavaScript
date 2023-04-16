86322bfd9b1947386ba3037c3db40ebebd46996c
GitHalex <calex1731@gmail.com>
2023-04-15 20:11:25 -0400

    Agregando archivos del Dia 2

---
+++ Dia2/tipos_datos.js
@@ -0,0 +1,18 @@
+// NIVEL 1
+let desafio = "30 Dias de JavaScript";
+
+console.log(desafio);
+console.log(desafio.length);
+console.log(desafio.toUpperCase());
+console.log(desafio.toLowerCase());
+console.log(desafio.substring(0));
+console.log(desafio.substring(3));
+console.log(desafio.includes("Script"));
+console.log(desafio.split());
+console.log(desafio.split(" "));
+
+let company = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+let matriz_company = company.split(",");
+
+console.log(matriz_company);
+console.log(desafio.replace("JavaScript", "Python"));
