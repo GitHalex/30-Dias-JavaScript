fecccb2c5ee53b502e7f6c231e282e6cbfe0dbb5
GitHalex <calex1731@gmail.com>
2023-04-20 19:30:24 -0400

    agregando arhivos del dia 3

---
+++ Dia3/unoNivel.js
@@ -0,0 +1,20 @@
+let firstName = "Alex";
+let lastName = "Calcina";
+let country = "Bolivia";
+let city = "Cochabamba";
+let age = 28;
+let isMarried = false;
+let year = 1994;
+console.log(typeof firstName);
+console.log(typeof lastName);
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof age);
+console.log(typeof isMarried);
+console.log(typeof year);
+
+console.log(typeof "10");
+console.log(typeof 10);
+console.log(typeof parseInt("9.8") == 10);
+
+console.log();
