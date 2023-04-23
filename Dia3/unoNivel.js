eda8c174709d50a5276da51d91dbddd7c9274563
GitHalex <calex1731@gmail.com>
2023-04-22 22:14:18 -0400

    Ejercicios de nivel 1 terminado

--- Dia3/unoNivel.js
+++ Dia3/unoNivel.js
@@ -1,4 +1,4 @@
-let firstName = "Alex";
+/* let firstName = "Alex";
 let lastName = "Calcina";
 let country = "Bolivia";
 let city = "Cochabamba";
@@ -15,6 +15,48 @@ console.log(typeof year);
 
 console.log(typeof "10");
 console.log(typeof 10);
-console.log(typeof parseInt("9.8") == 10);
+console.log(typeof parseInt("9.8") == 10); */
 
-console.log();
+/* console.log(4 > 3);
+console.log(4 >= 3);
+console.log(4 < 3);
+console.log(4 <= 3);
+console.log(4 == 4);
+console.log(4 === 4);
+console.log(4 != 4);
+console.log(4 !== 4);
+console.log(4 != "4");
+console.log(4 !== "4");
+console.log(4 === "4"); */
+
+/* let tamPython = "Python";
+let tamjargon = "jargon";
+console.log(tamPython.length != tamjargon.length); */
+
+/* console.log(4 > 3 && 10 < 12);
+console.log(4 > 3 && 10 > 12);
+console.log(4 > 3 || 10 > 12);
+console.log(4 > 3 || 10 > 12);
+console.log(!(4 > 3));
+console.log(!(4 < 3));
+console.log(!false);
+console.log(!(4 > 3 && 10 < 12));
+console.log(!(4 > 3 && 10 > 12));
+console.log(!(4 === "4")); */
+
+/* let name1 = "dragon";
+let name2 = "python";
+console.log(!name1.endsWith("on"));
+console.log(!name2.endsWith("on")); */
+
+let now = new Date();
+console.log(now);
+console.log(`el año es: ${now.getFullYear()}`);
+console.log(`el mes es: ${now.getMonth() + 1}`);
+console.log(`hoy es ${now.getDay()}`);
+console.log(`hora actual es: ${now.getHours()}`);
+console.log(`minutos es: ${now.getMinutes()}`);
+let milisegundos = now.getTime();
+console.log(milisegundos);
+
+console.log(`en segundos es ${milisegundos / 1000}`);
