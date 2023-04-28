d8959f9a590d9e54d702c67342e6fcc542b4d921
GitHalex <calex1731@gmail.com>
2023-04-28 01:37:09 -0400

    archivos agrgados

---
+++ Dia4/dosNivel.js
@@ -0,0 +1,14 @@
+/* let puntaje = parseInt(prompt("Ingrese un puntaje: "));
+if (puntaje >= 90 && puntaje <= 100) {
+  console.log("A");
+} else if (puntaje >= 70 && puntaje <= 89) {
+  console.log("B");
+} else if (puntaje >= 60 && puntaje <= 69) {
+  console.log("C");
+} else if (puntaje >= 50 && puntaje <= 59) {
+  console.log("D");
+} else if (puntaje >= 0 && puntaje <= 490) {
+  console.log("F");
+} else {
+  console.log("es un numero negativo");
+} */
--- Dia4/unoNivel.js
+++ Dia4/unoNivel.js
@@ -1,16 +1,26 @@
-console.log("hola");
-let num = prompt("Ingrese un número");
-console.log(typeof num);
-switch (true) {
-  case num > 0:
-    console.log("El número es positivo");
-    break;
-  case num == 0:
-    console.log("El número es cero");
-    break;
-  case num < 0:
-    console.log("El número es negativo");
-    break;
-  default:
-    console.log("El valor ingresado no era un número");
-}
+/* let edad = prompt("Ingrese su edad: ");
+edad >= 18
+  ? console.log("tiene la edad suficiente para conducir. ")
+  : console.log(`Te faltan ${18 - edad} años para conducir`);
+ */
+
+/* let miEdad = prompt("Ingresar mi edad: ");
+let tuEdad = prompt("Ingresa tu edad: ");
+if (miEdad > tuEdad) {
+  console.log(`Yo soy ${miEdad - tuEdad} mayor que tu`);
+} else if (tuEdad > miEdad) {
+  console.log(`Eres ${tuEdad - miEdad} mayor que yo`);
+} else {
+  console.log("Son iguales");
+} */
+
+/* let a = 4;
+let b = 76;
+a > b
+  ? console.log(`${a} es mayor que ${b}`)
+  : console.log(`${b} es mayor que ${a}`); */
+
+/* let numero = parseInt(prompt("Ingrese un número: "));
+numero % 2 == 0
+  ? console.log(`${numero} es un numero par`)
+  : console.log(`${numero} es un numero impar`); */
