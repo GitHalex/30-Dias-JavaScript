ad261e5c03d25a90daeaa19a32fb3f7e8992ed14
GitHalex <calex1731@gmail.com>
2023-04-27 22:04:01 -0400

    Ejercicios de nivel 2 terminados

--- Dia4/dosNivel.js
+++ Dia4/dosNivel.js
@@ -12,3 +12,33 @@ if (puntaje >= 90 && puntaje <= 100) {
 } else {
   console.log("es un numero negativo");
 } */
+
+/* let mes = prompt("Ingrese un mes: ");
+if (mes == "Septiembre" || mes == "Octubre" || mes == "Noviembre ") {
+  console.log("La temporada es Otoño");
+} else if (mes == "Diciembre" || mes == "Enero" || mes == "Febrero") {
+  console.log("La temporada es Invierno");
+} else if (mes == "Marzo" || mes == "Abril" || mes == "Mayo") {
+  console.log("La temporada es Primavera");
+} else if (mes == "Junio" || mes == "Julio" || mes == "Agosto") {
+  console.log("La temporada es Verano");
+} else {
+  console.log("Me pusiste cualquier huevada");
+}
+ */
+
+/* let dia = prompt("Que dia es hoy?: ");
+let day = dia.toLowerCase();
+if (day == "sabado" || day == "domingo") {
+  console.log(`El ${day} es fin de semana`);
+} else if (
+  day == "lunes" ||
+  day == "martes" ||
+  day == "miercoles" ||
+  day == "jueves" ||
+  day == "viernes"
+) {
+  console.log(`El ${day} es un dia laborable`);
+} else {
+  console.log(`Introduciste ${day} que no es un dia`);
+} */
