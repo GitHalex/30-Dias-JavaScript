f6f0f309a9e6e8a14071a79588df20cb0da49824
GitHalex <calex1731@gmail.com>
2023-04-29 18:54:38 -0400

    Ejercicios primera parte terminado

---
+++ Dia5/unoNivel.js
@@ -0,0 +1,64 @@
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+let vacio = [];
+console.log(vacio);
+
+const numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers);
+
+console.log(numbers.length);
+
+console.log(
+  `el primer elemento ${numbers[0]} - el elemento del medio ${numbers[3]} - el ultimo elemento ${numbers[4]}`
+);
+
+const mixedDataTypesPrimitivos = [
+  "string",
+  "number",
+  "boolean",
+  "null",
+  "undefined",
+  "NaN",
+];
+
+console.log(`longitud del array es ${mixedDataTypesPrimitivos.length}`);
+
+const itCompanies = [
+  "Facebook",
+  "Google",
+  "Microsoft",
+  "Apple",
+  "IBM",
+  "Oracle",
+  "Amazon",
+];
+console.log(itCompanies);
+
+console.log(`Numero de empresas ${itCompanies.length}`);
+
+console.log(
+  `primer empresa ${itCompanies[0]} ----- empresa intermedia ${itCompanies[3]} ----------- ultima empresa ${itCompanies[6]}`
+);
