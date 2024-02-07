124833548e4c94d98ac19eceac33d77b4d6f1d96
GitHalex <calex1731@gmail.com>
2024-02-06 21:45:36 -0400

    comentarios de los ejercicios terminados

--- Dia10/unoNivel.js
+++ Dia10/unoNivel.js
@@ -2,9 +2,11 @@ const a = [4, 5, 8, 9];
 const b = [3, 4, 5, 7];
 const countries = ["Finland", "Sweden", "Norway"];
 
+//  #1 crear un set vacío
 /* const vacio = new Set();
 console.log(vacio); */
 
+// # 2 Crear un set que contenga de 0 a 10 utilizando el bucle
 const num10 = new Set();
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (const num of numeros) {
@@ -12,11 +14,15 @@ for (const num of numeros) {
 }
 console.log(num10);
 
+// # 3 Eliminar un elemento de set
 console.log(num10.delete(10));
 console.log(num10);
 
+// #4 Limpia set
 num10.clear();
 console.log(num10);
+
+// #5 Crear un set de 5 elementos string a partir de un array
 
 const arrayStrings = ["Hola", "Como", "Estas", "Bien", "Mal"];
 const strings = new Set();
@@ -25,6 +31,7 @@ for (const stringa of arrayStrings) {
 }
 console.log(strings);
 
+// #6 Crear un map de países y el número de caracteres de un país
 const mapCountries = new Map();
 
 countriesPais = [
