0f46ae98e52d4773c209f8da4f1d3fac35afc5c4
GitHalex <calex1731@gmail.com>
2024-02-06 23:57:05 -0400

    Dia11 nivel uno terminado

--- Dia11/unoNivel.js
+++ Dia11/unoNivel.js
@@ -1,52 +1,19 @@
+// # 1Desestructurar y asignar los elementos del array de constantes para e, pi, gravedad, humanBodyTemp, waterBoilingTemp.
 const constants = [2.72, 3.14, 9.81, 37, 100];
+let [e, pi, gravedad, humanBodyTemp, waterBoilingTemp] = constants;
+console.log(e, pi, gravedad, humanBodyTemp, waterBoilingTemp);
+
+// #2 Desestructurar y asignar los elementos del array de países a fin, est, sw, den, nor
 const countries = ["Finland", "Estonia", "Sweden", "Denmark", "Norway"];
+let [fin, est, sw, den, nor] = countries;
+console.log(fin, est, sw, den, nor);
+
+// #3 Desestructurar el objeto rectángulo por sus propiedades o keys.
 const rectangle = {
   width: 20,
   height: 10,
   area: 200,
   perimeter: 60,
 };
-const users = [
-  {
-    name: "Brook",
-    scores: 75,
-    skills: ["HTM", "CSS", "JS"],
-    age: 16,
-  },
-  {
-    name: "Alex",
-    scores: 80,
-    skills: ["HTM", "CSS", "JS"],
-    age: 18,
-  },
-  {
-    name: "David",
-    scores: 75,
-    skills: ["HTM", "CSS"],
-    age: 22,
-  },
-  {
-    name: "John",
-    scores: 85,
-    skills: ["HTML"],
-    age: 25,
-  },
-  {
-    name: "Sara",
-    scores: 95,
-    skills: ["HTM", "CSS", "JS"],
-    age: 26,
-  },
-  {
-    name: "Martha",
-    scores: 80,
-    skills: ["HTM", "CSS", "JS"],
-    age: 18,
-  },
-  {
-    name: "Thomas",
-    scores: 90,
-    skills: ["HTM", "CSS", "JS"],
-    age: 20,
-  },
-];
+let { width, height, area, perimeter } = rectangle;
+console.log(width, height, area, perimeter);
