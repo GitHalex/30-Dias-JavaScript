f76b77309a1358514d05bf57a8555d7c0f94e392
GitHalex <calex1731@gmail.com>
2024-02-13 22:21:31 -0400

    agregando archicos

--- Dia13/dosNivel.js
+++ Dia13/dosNivel.js
@@ -1,10 +1,10 @@
-console.assert(10 > 2 * 10, "10 is not greater than 20"); // no hay resultado
-console.assert(3 > 4, "3 is not greater than 4"); // Aserción fallida: 3 no es mayor que 4
+const myArray = [];
+let i = 10;
 
-for (let i = 0; i <= 10; i += 1) {
-  let errorMessage = `${i} is not even`;
-  console.log("the # is " + i);
-  console.assert(i % 2 === 0, { number: i, errorMessage: errorMessage });
-}
-console.warn("Este es una advertencia");
-console.error("Este es una error");
+// Cambia solo el código debajo de esta línea
+do {
+  myArray.push(i);
+  i++;
+} while (i <= 11);
+
+console.log(myArray);
