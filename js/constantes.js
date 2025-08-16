651f741ba4e46393bc7fce065935050630752155
GitHalex <calex1731@gmail.com>
2025-08-16 16:36:30 -0400

    Added a new fiule

---
+++ js/constantes.js
@@ -0,0 +1,8 @@
+export const PI = Math.PI;
+console.log(PI);
+
+export const arreglo = ["Alex", "Calcina", "Bernal"];
+
+export default function saludar() {
+  console.log("Hola Módulos +ES6");
+}
---
+++ js/modulos.js
@@ -0,0 +1,8 @@
+import { aritmetica } from "./aritmetica.js";
+import saludar, { PI } from "./constantes.js";
+
+console.log("Archivos modulos");
+console.log(PI);
+console.log(aritmetica.sumar(4, 2));
+console.log(aritmetica.restar(4, 2));
+saludar();
