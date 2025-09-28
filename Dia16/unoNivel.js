2ef2f5cfce171fe421d00cc90430ff8a2bab9268
GitHalex <calex1731@gmail.com>
2025-09-27 23:47:52 -0400

    Added: new file

--- Dia16/unoNivel.js
+++ Dia16/unoNivel.js
@@ -13,3 +13,30 @@ let twinkleStar = "Twinkle, twinkle, little star";
 let starRegex = /Twinkle|start/gi; // Cambia esta línea
 let result = twinkleStar.match(starRegex); // Cambia esta línea
 console.log(result);
+
+const usersText = `{
+"users":[
+  {
+    "firstName":"Asabeneh",
+    "lastName":"Yetayeh",
+    "age":250,
+    "email":"asab@asb.com"
+  },
+  {
+    "firstName":"Alex",
+    "lastName":"James",
+    "age":25,
+    "email":"alex@alex.com"
+  },
+  {
+  "firstName":"Lidiya",
+  "lastName":"Tekle",
+  "age":28,
+  "email":"lidiya@lidiya.com"
+  }
+]
+}`;
+console.log(typeof usersText);
+
+const usersObj = JSON.parse(usersText, undefined, 4);
+console.log(usersObj);
